d7196173e3dea5e87d5daf860c3c12bf9bb8a45e
Eman Khaled <eman.khismaeil@gmail.com>
2021-06-03 22:52:18 +0200

    TurnOn_LED_100

---
+++ PORTF.c
@@ -0,0 +1,36 @@
+# include "PORTF.h"
+void PORTF_Init(void)
+{
+	volatile unsigned long delay;
+	SYSCTL_RCGCGPIO_R |=0X20;
+	delay = SYSCTL_RCGCGPIO_R;
+	//while ((SYSCTL_PRGPIO_R & 0X20) == 0);
+	GPIO_PORTF_LOCK_R = 0X4C4F434B;
+	GPIO_PORTF_CR_R |= 0X1F;
+	GPIO_PORTF_DIR_R |= 0X0E;
+	GPIO_PORTF_PUR_R |= 0X11;
+	GPIO_PORTF_DEN_R |= 0X0E;
+	GPIO_PORTF_AMSEL_R = 0X00;
+	GPIO_PORTF_AFSEL_R = 0X00;
+	GPIO_PORTF_PCTL_R = 0X00;
+}
+
+void PORTF_Output(uint8_t data)
+{
+	GPIO_PORTF_DATA_R &=~0X0E;
+	GPIO_PORTF_DATA_R |= data;	
+}
+
+uint8_t PORTF_Input(void)
+{
+	return (GPIO_PORTF_DATA_R & 0X11);
+}
+
+void turn_on_Led_100(uint32_t distance) {
+
+	if (distance > 100) {
+		PORTF_Output(0x02);
+
+	}
+}
+
---
+++ PORTF.h
@@ -0,0 +1,12 @@
+# ifndef FILES_H
+# define FILES_H
+# include "tm4c123gh6pm.h"
+# include "stdint.h"
+# include "string.h"
+# endif
+
+	
+void PORTF_Init(void);
+void PORTF_Output(uint8_t data);
+uint8_t PORTF_Input(void);
+void turn_on_Led_100(uint32_t distance);
