e05a68e17645b5643babd561fbd5035f0bd4010a
john <jijo3mad@gmail.com>
2021-06-05 15:18:03 +0200

    Reading Coordinates from text file then Drawing

--- Plotting Trajectory/Drawing_Trajectory.m
+++ Plotting Trajectory/Drawing_Trajectory.m
@@ -3,12 +3,19 @@ clear all
 wmremove();
 
 %read trajectories from file in float and so on
+fileID = fopen('nums1.txt','r');
+sizeA = [2 Inf];
+formatSpec = '%f %f';
+A=fscanf(fileID,formatSpec,sizeA);
+fclose(fileID);
 %then putting it in two row vectors
-
+B=A';
+L=length(B);
+Lat=B(1:L);
+Long=B(L+1:end);
 %test initialize coordinates matrix
-lat = [30.086179922289784 30.08375812841175  30.079179099142745];
-long = [31.335412434388285 31.340769430551084 31.339726989069494];
-
+%lat = [30.086179922289784 30.08375812841175  30.079179099142745];
+%long = [31.335412434388285 31.340769430551084 31.339726989069494];
 
 %using webmap plotting
-wmline(lat, long);
+wmline(Lat, Long);
