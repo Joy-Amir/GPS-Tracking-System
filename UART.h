270d5e5a1a252129ee4f8f55a935357ce5b62cd2
Habiba1998 <54662472+Habiba1998@users.noreply.github.com>
2021-06-09 12:32:33 -0700

    Coordinates Extraction and UART fns

--- UART.h
+++ UART.h
@@ -8,3 +8,8 @@
 void UART0_Init(void);
 uint8_t UART0_InChar(void);
 void UART2_Init(void);
+void output(char* latitude, char*longitude, char latitude_type, char longitude_type);
+uint8_t UART2_InChar(void);
+void UART2_OutChar(uint8_t data);
+void UART0_OutChar(uint8_t data);
+void get_coordinates(void);
