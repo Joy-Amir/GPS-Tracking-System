1102b795bfe52f6a03e8cc8c18a1a3b6c37ed54d
Habiba1998 <54662472+Habiba1998@users.noreply.github.com>
2021-06-10 10:44:04 -0700

    update get_coordinates fn

--- UART.h
+++ UART.h
@@ -1,18 +1,3 @@
-/*# ifndef FILES_H
-# define FILES_H
-# include "tm4c123gh6pm.h"
-# include "stdint.h"
-# include "string.h"
-# endif
-# include "PORTF.h"
-# include "Systick timer.h"
-# include "LCD.h"
-
-void UART0_Init(void);
-uint8_t UART0_InChar(void);
-void UART0_OutChar(uint8_t data);*/
-
-
 # ifndef FILES_H
 # define FILES_H
 # include "tm4c123gh6pm.h"
@@ -27,4 +12,4 @@ void output(char* latitude, char*longitude, char latitude_type, char longitude_t
 uint8_t UART2_InChar(void);
 void UART2_OutChar(uint8_t data);
 void UART0_OutChar(uint8_t data);
-void get_coordinates(char* latitude,char* longitude, char* latitude_type, char* longitude_type);
+uint8_t get_coordinates(char* latitude,char* longitude, char* latitude_type, char* longitude_type);
